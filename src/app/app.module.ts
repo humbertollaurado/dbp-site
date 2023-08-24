f7d97f609d15d8ba866d8af5368421784234d17b
bscelza11 <barbarascelza11@gmail.com>
2023-08-24 19:36:39 +0000

    fix: create navbar component and test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, CoreModule],
 })
 export class AppModule {}
