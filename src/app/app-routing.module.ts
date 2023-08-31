24d37a76fcb8beba2023d42b3824ad1a2f3b0204
bscelza11 <barbarascelza11@gmail.com>
2023-08-31 20:33:40 +0000

    feat: create routing app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,52 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'about',
+    loadChildren: () =>
+      import('./feature/about/about.module').then(m => m.AboutModule),
+  },
+  {
+    path: 'case-study',
+    loadChildren: () =>
+      import('./feature/case-studies/case-studies.module').then(
+        m => m.CaseStudiesModule
+      ),
+  },
+  {
+    path: 'contact',
+    loadChildren: () =>
+      import('./feature/contact/contact.module').then(m => m.ContactModule),
+  },
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./feature/home/home.module').then(m => m.HomeModule),
+  },
+  {
+    path: 'category/insights',
+    loadChildren: () =>
+      import('./feature/insights/insights.module').then(m => m.InsightsModule),
+  },
+  {
+    path: 'services',
+    loadChildren: () =>
+      import('./feature/page-services/page-services.module').then(
+        m => m.PageServicesModule
+      ),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
